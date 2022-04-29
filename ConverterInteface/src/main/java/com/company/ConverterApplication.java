b0e9e4f28d4759c633973d35afe0d11edfb8bd3b
Michael Klein <Michaelklein1997@gmail.com>
2022-04-29 13:21:44 -0500

    added methods to main app

--- ConverterInteface/src/main/java/com/company/ConverterApplication.java
+++ ConverterInteface/src/main/java/com/company/ConverterApplication.java
@@ -3,7 +3,24 @@ package com.company;
 public class ConverterApplication {
 
     public static void main(String[] args) {
+        ConverterIf converterIf = new ConverterIf();
+        ConverterSwitch converterSwitch = new ConverterSwitch();
+
+        System.out.println("==============================================");
+
+        String switchMonth = converterSwitch.convertMonth(2);
+        String switchDay = converterSwitch.convertDay(2);
+
+        System.out.println("Using the ConverterSwitch class, convertMonth(2) and convertDay(2) output...\n" + switchMonth + ", \n" + switchDay);
+
+        System.out.println("==============================================");
+
+        String ifMonth = converterSwitch.convertMonth(12);
+        String ifDay = converterSwitch.convertDay(6);
+
+        System.out.println("Using the ConverterSwitch class, ifMonth(12) and ifDay(6) output...\n" + ifMonth + ", \n" + ifDay);
+
+        System.out.println("==============================================");
 
     }
-
 }
