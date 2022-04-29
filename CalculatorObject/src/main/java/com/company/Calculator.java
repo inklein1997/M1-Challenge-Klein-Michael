6323cfa7be09e9797387d61640db07993b38d571
Michael Klein <Michaelklein1997@gmail.com>
2022-04-29 17:22:53 -0500

    finalized calculator assignment

--- CalculatorObject/src/main/java/com/company/Calculator.java
+++ CalculatorObject/src/main/java/com/company/Calculator.java
@@ -2,35 +2,42 @@ package com.company;
 
 public class Calculator {
     public int add(int a, int b) {
+        System.out.println(a + " + " + b + " = " + (a + b));
         return a + b;
     }
 
     public double add(double a, double b) {
+        System.out.println(a + " + " + b + " = " + (a + b));
         return a + b;
     }
 
     public int subtract(int a, int b) {
+        System.out.println(a + " - " + b + " = " + (a - b));
         return a - b;
     }
 
     public double subtract(double a, double b) {
+        System.out.println(a + " - " + b + " = " + (a - b));
         return a - b;
     }
 
     public int multiply(int a, int b) {
+        System.out.println(a + " * " + b + " = " + (a * b));
         return a * b;
     }
 
     public double multiply(double a, double b) {
+        System.out.println(a + " * " + b + " = " + (a * b));
         return a * b;
     }
 
     public double divide(int a, int b) {
-        System.out.println(a / b);
+        System.out.println(a + " / " + b + " = " + (a / b));
         return (double) a / b;
     }
 
     public double divide (double a, double b) {
-         return a / b;
+        System.out.println(a + " / " + b + " = " + (a / b));
+        return a / b;
     }
 }
