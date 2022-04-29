4d30e2d43406d7215808118e39e8b6bdfceee014
Michael Klein <Michaelklein1997@gmail.com>
2022-04-29 17:16:44 -0500

    Created calculator class

--- CalculatorObject/src/test/java/com/company/CalculatorTest.java
+++ CalculatorObject/src/test/java/com/company/CalculatorTest.java
@@ -14,51 +14,51 @@ public class CalculatorTest {
     }
 
     @Test
-    private void shouldAddTwoIntegers() {
+    public void shouldAddTwoIntegers() {
         assertEquals(2, calculator.add(1,1));
         assertEquals(-8, calculator.add(-20,12));
     }
 
     @Test
-    private void shouldAddTwoDoubles() {
-        assertEquals(5.7, calculator.add(3.4,2.3));
-        assertEquals(32.50001, calculator.add(1.00001,31.5));
+    public void shouldAddTwoDoubles() {
+        assertEquals(5.7, calculator.add(3.4,2.3), 0.01);
+        assertEquals(32.51, calculator.add(1.01,31.5), 0.01);
     }
 
     @Test
-    private void shouldSubtractTwoIntegers() {
+    public void shouldSubtractTwoIntegers() {
         assertEquals(-29, calculator.subtract(23,52));
         assertEquals(0, calculator.subtract(1000,1000));
     }
 
     @Test
-    private void shouldSubtractTwoDoubles() {
-        assertEquals(5.0, calculator.subtract(5.5,0.5));
-        assertEquals(0.1, calculator.subtract(-5.1,-5.2));
+    public void shouldSubtractTwoDoubles() {
+        assertEquals(5.0, calculator.subtract(5.5,0.5), 0.01);
+        assertEquals(0.1, calculator.subtract(-5.1,-5.2), 0.01);
     }
 
     @Test
-    private void shouldMultiplyTwoIntegers() {
+    public void shouldMultiplyTwoIntegers() {
         assertEquals(68, calculator.multiply(34,2));
         assertEquals(1000000, calculator.multiply(1000,1000));
     }
 
     @Test
-    private void shouldMultipleTwoDoubles() {
-        assertEquals(29.48, calculator.multiply(6.7,4.4));
-        assertEquals(8.47, calculator.multiply(7.7,1.1));
+    public void shouldMultipleTwoDoubles() {
+        assertEquals(29.48, calculator.multiply(6.7,4.4), 0.01);
+        assertEquals(8.47, calculator.multiply(7.7,1.1), 0.01);
     }
 
     @Test
-    private void shouldDivideTwoIntegers() {
-        assertEquals(1.714, calculator.divide(12,7));
-        assertEquals(4, calculator.divide(12,3));
+    public void shouldDivideTwoIntegers() {
+        assertEquals(1.714, calculator.divide(12,7), 0.001);
+        assertEquals(4, calculator.divide(12,3), 0.001);
 
     }
 
     @Test
-    private void shouldDivideTwoDoubles() {
-        assertEquals(49.09, calculator.divide(10.8,2.2));
-        assertEquals(442.482, calculator.divide(1000.01,2.26));
+    public void shouldDivideTwoDoubles() {
+        assertEquals(4.909, calculator.divide(10.8,2.2), 0.001);
+        assertEquals(442.482, calculator.divide(1000.01,2.26), 0.001);
     }
 }
